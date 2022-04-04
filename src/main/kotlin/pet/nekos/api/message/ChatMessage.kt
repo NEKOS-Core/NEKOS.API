af1d42c1f3f54926304a1afbbd26f1bc8aed80c8
diademiemi <emilia@diademiemi.me>
2022-04-04 16:38:37 +0200

    make reply boolean

--- src/main/kotlin/pet/nekos/api/message/ChatMessage.kt
+++ src/main/kotlin/pet/nekos/api/message/ChatMessage.kt
@@ -1,12 +1,14 @@
 package pet.nekos.api.message
 
 import pet.nekos.api.user.User
+import pet.nekos.api.channel.Channel
 
 open class ChatMessage(
     content: String,
-    user: User
-): Message(content, user) {
-    open fun reply(content: String) {
-
+    user: User,
+    channel: Channel
+): Message(content, user, channel) {
+    open fun reply(content: String): Boolean {
+        return false
     }
 }
