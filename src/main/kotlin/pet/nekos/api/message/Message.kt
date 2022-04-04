579d22877fbb94adf6c1384a93235bc6b131bbc5
diademiemi <emilia@diademiemi.me>
2022-04-04 16:38:10 +0200

    use channel on chat message

--- src/main/kotlin/pet/nekos/api/message/Message.kt
+++ src/main/kotlin/pet/nekos/api/message/Message.kt
@@ -1,8 +1,10 @@
 package pet.nekos.api.message
 
 import pet.nekos.api.user.User
+import pet.nekos.api.channel.Channel
 
 open class Message (
     var content: String,
-    var user: User
+    var user: User,
+    var channel: Channel
 ) { }
