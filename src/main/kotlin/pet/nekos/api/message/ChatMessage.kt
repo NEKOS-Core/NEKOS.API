ef81512c448631d0fe12743e0e1c1c2eba7cbc22
diademiemi <emilia@diademiemi.me>
2022-04-10 01:49:11 +0200

    Add isOwn function

--- src/main/kotlin/pet/nekos/api/message/ChatMessage.kt
+++ src/main/kotlin/pet/nekos/api/message/ChatMessage.kt
@@ -2,13 +2,27 @@ package pet.nekos.api.message
 
 import pet.nekos.api.user.User
 import pet.nekos.api.channel.Channel
+import pet.nekos.api.service.Service
+import pet.nekos.api.service.ChatService
 
 open class ChatMessage(
     content: String,
     user: User,
-    channel: Channel
-): Message(content, user, channel) {
+    channel: Channel,
+    service: ChatService
+): Message(content, user, channel, service as Service) {
+
+    open fun isOwn(): Boolean {
+        if (service.getSelfUser() == null) {
+            return true
+        } else if (service.getSelfUser()?.hash == user.hash) {
+            return true
+        }
+        return false
+    }
+
     open fun reply(content: String): Boolean {
         return false
     }
+
 }
