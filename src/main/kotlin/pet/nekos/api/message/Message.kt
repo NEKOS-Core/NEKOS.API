d39106cc7a09ea7590858975eaa36b6a2bf58f8f
diademiemi <emilia@diademiemi.me>
2022-04-10 01:49:23 +0200

    Include service in message

--- src/main/kotlin/pet/nekos/api/message/Message.kt
+++ src/main/kotlin/pet/nekos/api/message/Message.kt
@@ -2,9 +2,11 @@ package pet.nekos.api.message
 
 import pet.nekos.api.user.User
 import pet.nekos.api.channel.Channel
+import pet.nekos.api.service.Service
 
 open class Message (
     var content: String,
     var user: User,
-    var channel: Channel?
+    var channel: Channel?,
+    var service: Service
 ) { }
