69a5ea6ba17ddc086c9f9111588a5266e5a28127
diademiemi <emilia@diademiemi.me>
2022-04-10 02:11:21 +0200

    Document messages

--- src/main/kotlin/pet/nekos/api/message/ChatMessage.kt
+++ src/main/kotlin/pet/nekos/api/message/ChatMessage.kt
@@ -5,22 +5,26 @@ import pet.nekos.api.channel.Channel
 import pet.nekos.api.service.Service
 import pet.nekos.api.service.ChatService
 
+/**
+ * Represents a chat message. Chat messages can be replied to
+ * @property content Content of the message
+ * @property user User that sent this message
+ * @property channel Channel this message was sent in (can be null)
+ * @property service Service that this message originates from
+ */
 open class ChatMessage(
     content: String,
     user: User,
-    channel: Channel,
+    channel: Channel?,
     service: ChatService
 ): Message(content, user, channel, service as Service) {
 
-    open fun isOwn(): Boolean {
-        if (service.getSelfUser() == null) {
-            return true
-        } else if (service.getSelfUser()?.hash == user.hash) {
-            return true
-        }
-        return false
-    }
-
+    /**
+     * Reply to this message
+     * 
+     * @param content Content of the message to send
+     * @return If successful
+     */
     open fun reply(content: String): Boolean {
         return false
     }
--- src/main/kotlin/pet/nekos/api/message/Message.kt
+++ src/main/kotlin/pet/nekos/api/message/Message.kt
@@ -4,9 +4,32 @@ import pet.nekos.api.user.User
 import pet.nekos.api.channel.Channel
 import pet.nekos.api.service.Service
 
+/**
+ * Represents a chat message. Chat messages can be replied to
+ * @property content Content of the message
+ * @property user User that sent this message
+ * @property channel Channel this message was sent in (can be null)
+ * @property service Service that this message originates from
+ */
 open class Message (
     var content: String,
     var user: User,
     var channel: Channel?,
     var service: Service
-) { }
+) {
+
+    /**
+     * Check if this message belongs to the user the bot is running as
+     * 
+     * @return If the message is from the bot itself
+     */
+    open fun isOwn(): Boolean {
+        if (service.getSelfUser() == null) {
+            return true
+        } else if (service.getSelfUser()?.hash == user.hash) {
+            return true
+        }
+        return false
+    }
+
+}
