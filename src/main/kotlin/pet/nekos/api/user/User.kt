c8a617f060014d6df05faed31d3cd4d9683b1ffe
diademiemi <emilia@diademiemi.me>
2022-04-10 02:31:54 +0200

    Document user

--- src/main/kotlin/pet/nekos/api/user/User.kt
+++ src/main/kotlin/pet/nekos/api/user/User.kt
@@ -1,5 +1,10 @@
 package pet.nekos.api.user
 
+/** Class that represents a user
+ * @property name Name of this user
+ * @property nickname The friendly name of this user, defaults to the same as name
+ * @propery hash A hash of permmanent identifiable information for this user. This can be used to verify if user objects are equal
+ */
 open class User (
     var name: String,
     var nickname: String = name,
