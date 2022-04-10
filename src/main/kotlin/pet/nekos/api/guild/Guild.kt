817f8287628efbe3a031867e08b6ab2577cbd174
diademiemi <emilia@diademiemi.me>
2022-04-10 02:01:28 +0200

    Document guild

--- src/main/kotlin/pet/nekos/api/guild/Guild.kt
+++ src/main/kotlin/pet/nekos/api/guild/Guild.kt
@@ -2,6 +2,11 @@ package pet.nekos.api.guild
 
 import pet.nekos.api.channel.Channel
 
+/**
+ * Represents a guild in NEKOS
+ * A guild is mainly just a grouping of channels
+ * @property name Name for this guild
+ */
 abstract class Guild (
     var name: String,
 ) {
